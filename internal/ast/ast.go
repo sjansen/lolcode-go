9831baee60ee7dd2e8bf716ec8d92947c4d46eea
Stuart Jansen <sjansen@buscaluz.org>
2020-03-19 08:41:16 -0600

    replace ragel 7 with ragel 6

--- internal/ast/ast.go
+++ internal/ast/ast.go
@@ -2,7 +2,7 @@ package ast
 
 import "io"
 
-//go:generate ragel-go -G2 -o ast_parser.go ast_parser.rl
+//go:generate ragel -Z -G2 -o ast_parser.go ast_parser.rl
 
 type Program struct {
 	Statements []Statement
